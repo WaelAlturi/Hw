8785e39467918c2c936d1521753d06651c3dcd45
WaelAlturi <101098027+WaelAlturi@users.noreply.github.com>
2022-11-30 11:14:59 +0200

    301122

--- people.js
+++ people.js
@@ -2,21 +2,31 @@ import React from 'react';
 import { Text,View,StyleSheet,Image } from 'react-native';
 
 const People = (props) =>{
+  let {AgeFromDate} = require('age-calculator');
+  let ageFromDate = new AgeFromDate(new Date(props.info.contact.birthday)).age;
+ 
+  
     return(
         <View style={myStyle.row}>
-            <View>
-            <Text>{props.info.name}</Text>
+            <Image style={myStyle.imgBB} source={{url:props.info.image}}/>
+            <View style={{margin:2}}>
+                <Text>{props.info.contact.firstname}</Text>
+                <Text>{props.info.country}</Text>
+                <Text>{ageFromDate}</Text>
             </View>
+            <Text>{props.info.contact.lastname}</Text>
+            <Text style={{marginLeft:20}}>{props.info.contact.gender}</Text>
         </View>
     )
 }
 
 const myStyle = StyleSheet.create({
     row:{
-        width:'100%',
-        backgroundColor:'#ffffff', borderRadius:8,
-        marginBottom:22, padding:20,marginTop:20
+        width:'100%',backgroundColor:'#ffffff', borderRadius:20,marginBottom:22, padding:10,marginTop:20,flexDirection:'row',
     },
+    imgBB:{
+        width:'30%',height:130,
+      },
 })
 
 
